e50e85df38f2e1192eaa54290e2018e2e99d2dbd
Rajesh G <29516506+dude-with-the-long-username@users.noreply.github.com>
2022-09-23 01:11:40 +0530

    ✨ feat: favorites current wallpaper from variety

--- favouriter.py
+++ favouriter.py
@@ -1,7 +1,9 @@
 import os
-from xml.dom import NotFoundErr
+import re
 from dotenv import load_dotenv
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import Playwright, sync_playwright
+import subprocess
+import tempfile
 
 #TODO: add requirements.txt file
 
@@ -38,13 +40,23 @@ def run(playwright: Playwright) -> None:
     page.locator("button:has-text(\"Login\")").click()
     page.wait_for_url(f"https://wallhaven.cc/user/{username}")
 
+    cmd = ['variety', '--get']  #command to run in terminal - prints path of the current wallpaper set by variety
+    with tempfile.TemporaryFile() as tempf:
+        proc = subprocess.Popen(cmd, stdout=tempf)
+        proc.wait()
+        tempf.seek(0)   # go to start of file
+        command_output : list = tempf.readlines()  # outputs a list
+    
+    # Example of command_output= b'/home/fiona/.config/variety/Downloaded/wallhaven_wallhaven_cc_search_q_like_3Ag7l5x3_categories_111_purity_100_sorting_relevance_order_desc/wallhaven-y8w9ex.jpg\n'
+    wallpaper_id : str = re.search(r"(.*wallhaven-)(.*?)\..*", str(command_output[0])).group(2)
+    
     # Go to https://wallhaven.cc/w/k7j1qd
-    page.goto("https://wallhaven.cc/w/k7j1qd")      # wallpaper url that we want to favourite
+    page.goto(f"https://wallhaven.cc/w/{wallpaper_id}")      # wallpaper url that we want to favourite
+    fav_button_text : str = page.locator('id=fav-button').inner_text()
 
-    a= page.locator('id=fav-button').inner_text()
-    if a == ' Add to Favorites':
+    if fav_button_text == ' Add to Favorites':
         page.locator('id=fav-button').click()
-    elif a == ' In Favorites':
+    elif fav_button_text == ' In Favorites':
         ...
 
     # ---------------------
